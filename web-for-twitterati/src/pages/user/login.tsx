2229133461fcedb2435820fd9c26b16218d69840
raisa0726 <chst130726@gmail.com>
2022-04-24 12:56:40 +0900

    css-update

--- web-for-twitterati/src/pages/user/login.tsx
+++ web-for-twitterati/src/pages/user/login.tsx
@@ -6,15 +6,19 @@ const Login: NextPage = () => {
       <h1>Login</h1>
       <form method="POST">
         <div className="form-group">
-          <label htmlFor="user_id" className="form-label">ユーザーID</label>
-          <input type="text" name="user_id" className="form-control" />
-          <br />
-          <label htmlFor="user_password" className="form-label">パスワード</label>
-          <input type="password" name="user_password" className="form-control" />
-          <br />
-          <button type="button" className="btn-submit">
-            <input type="submit" value="ログイン" />
-          </button>
+          <div className='form-child'>
+            <label htmlFor="user_id" className="form-label">ユーザーID</label>
+            <input type="text" name="user_id" className="form-control" />
+          </div>
+          <div className='form-child'>
+            <label htmlFor="user_password" className="form-label">パスワード</label>
+            <input type="password" name="user_password" className="form-control" />
+          </div>
+          <div className='form-child'>
+            <button type="button" className="btn-submit">
+              <input type="submit" value="ログイン" />
+            </button>
+          </div>
         </div>
       </form>
     </div>
--- web-for-twitterati/src/pages/user/signup.tsx
+++ web-for-twitterati/src/pages/user/signup.tsx
@@ -6,15 +6,19 @@ const SignUp: NextPage = () => {
       <h1>新規登録</h1>
       <form method="POST">
         <div className="form-group">
-          <label htmlFor="user_name" className="form-label">ユーザー名</label>
-          <input type="text" name="user_name" className="form-control" />
-          <br />
-          <label htmlFor="user_password" className="form-label">パスワード</label>
-          <input type="password" name="user_password" className="form-control" />
-          <br />
-          <button type="button" className="btn-submit">
-            <input type="submit" value="新規登録" />
-          </button>
+        <div className='form-child'>
+            <label htmlFor="user_name" className="form-label">ユーザー名</label>
+            <input type="text" name="user_name" className="form-control" />
+          </div>
+          <div className='form-child'>
+            <label htmlFor="user_password" className="form-label">パスワード</label>
+            <input type="password" name="user_password" className="form-control" />
+          </div>
+          <div className='form-child'>
+            <button type="button" className="btn-submit">
+              <input type="submit" value="新規登録" />
+            </button>
+          </div>
         </div>
       </form>
     </div>
